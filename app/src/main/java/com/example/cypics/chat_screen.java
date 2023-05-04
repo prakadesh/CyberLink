277d144e3366c42b34d827d09279afc36b7d7936
prakadesh <pragapadma123@gmail.com>
2023-05-04 14:08:01 +0530

    updated layout for chatbot ui and ml_model prediction compelete functionality

---
+++ app/src/main/java/com/example/cypics/chat_screen.java
@@ -0,0 +1,11 @@
+package com.example.cypics;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class chat_screen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_screen_layout);
+    }
+}
